031edce5212501b0b0ddb6d23e2e5e5e4fd0ad9e
Anant Saxena <anantsaxena.official@gmail.com>
2022-01-27 21:00:22 +0530

    Partial Code

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.getElementById("newMeet").addEventListener("click", createNewMeeting);
+document.getElementById("joinMeet").addEventListener("submit", joinMeeting);
+
 
 function createNewMeeting() {
+    console.log("creating NewMeeting");
     chrome.tabs.create({url : "http://meet.google.com/new"});
+}
+
+// document.getElementById("linkForm").addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     var link = document.getElementById("meetLink").value;
+//     console.log(link);
+//     chrome.tabs.create({url : link});
+// });
+
+function joinMeeting() {
+    var myInput = document.getElementById("meetDetails").value;
+    chrome.tabs.create({url : myInput});
 }
