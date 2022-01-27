6eff0da6a797704c8cc3636f07b7bb0d279f6319
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2022-01-28 05:29:37 +0530

    Added: Join with Link and Nickname

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-document.getElementById("newMeet").addEventListener("click", createNewMeeting);
-document.getElementById("joinMeet").addEventListener("submit", joinMeeting);
-
+document.getElementById("joinMeetForm").addEventListener("submit", joinMeeting);
 
 function createNewMeeting() {
-    console.log("creating NewMeeting");
     chrome.tabs.create({url : "http://meet.google.com/new"});
 }
 
-// document.getElementById("linkForm").addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     var link = document.getElementById("meetLink").value;
-//     console.log(link);
-//     chrome.tabs.create({url : link});
-// });
-
 function joinMeeting() {
-    var myInput = document.getElementById("meetDetails").value;
-    chrome.tabs.create({url : myInput});
+    var meetDetails = document.getElementById("meetDetails");
+    if(meetDetails.value.length == 0) {
+        createNewMeeting();
+        return;
+    }
+    chrome.tabs.create({url : "http://meet.google.com/lookup/" + meetDetails.value});
 }
