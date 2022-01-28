8eebea1abb2c423d47ff067737a59464b21984f3
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2022-01-28 18:27:12 +0530

    Added: Meeting Launches in same tab if activeTab==newTab

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 document.getElementById("joinMeetForm").addEventListener("submit", joinMeeting);
+var samePageMeetingLaunch = ["chrome://newtab", "edge://newtab", "about:blank"];
 
-function createNewMeeting() {
-    chrome.tabs.create({url : "http://meet.google.com/new"});
+function createNewMeeting(tabPromise) {
+    launchMeeting("http://meet.google.com/new", tabPromise);
 }
-function joinMeetingWithAlias(alias) {
-    chrome.tabs.create({url : "http://meet.google.com/lookup/" + alias});
+function joinMeetingWithAlias(alias, tabPromise) {
+    launchMeeting("http://meet.google.com/lookup/" + alias, tabPromise);
 }
-function joinMeetingWithURL(url) {
-    chrome.tabs.create({url : url});
+function joinMeetingWithURL(url, tabPromise) {
+    launchMeeting(url, tabPromise);
 }
 
+// this function checks if active tab is in samePageMeetingLaunch, if yes, url is opened in activeTab
+// if not, new tab is created with url
+function launchMeeting(url, tabPromise) {
+    tabPromise.then(function (tabs) {
+        var tabUrl = tabs[0].url;
+        if (samePageMeetingLaunch.some((page) => tabUrl.startsWith(page))) {
+            chrome.tabs.update(tabs[0].id, { url: url });
+        }
+        else {
+            chrome.tabs.create({ url: url });
+        }
+    }, function (error) {
+        console.log("Fetching active tab failed: " + error);
+        chrome.tabs.create({ url: url });
+    });
+}
 function joinMeeting() {
+    var tabPromise = chrome.tabs.query({ active: true, lastFocusedWindow: true});
     var meetDetails = document.getElementById("meetDetails");
-    if(meetDetails.value.length == 0) {
-        createNewMeeting();
-        return;
+    if (meetDetails.value.length == 0) {
+        createNewMeeting(tabPromise);
     }
-    if(meetDetails.value.match(/^[a-z0-9-]+$/i) != null) {
-        joinMeetingWithAlias(meetDetails.value);
-        return;
+    else if (meetDetails.value.match(/^[a-z0-9-]+$/i) != null) {
+        joinMeetingWithAlias(meetDetails.value, tabPromise);
     }
-    if(meetDetails.value.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/) != null) {
-        if(meetDetails.value.includes("meet.google.com"))
-        {
-            joinMeetingWithURL(meetDetails.value);
+    else if (meetDetails.value.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/) != null) {
+        if (meetDetails.value.includes("meet.google.com")) {
+            joinMeetingWithURL(meetDetails.value, tabPromise);
         }
     }
 }
