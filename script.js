10008b802dcb2067908ad919d97a92bdce309d7f
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2022-01-28 14:54:00 +0530

    fix joining with URL

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ document.getElementById("joinMeetForm").addEventListener("submit", joinMeeting);
 function createNewMeeting() {
     chrome.tabs.create({url : "http://meet.google.com/new"});
 }
+function joinMeetingWithAlias(alias) {
+    chrome.tabs.create({url : "http://meet.google.com/lookup/" + alias});
+}
+function joinMeetingWithURL(url) {
+    chrome.tabs.create({url : url});
+}
 
 function joinMeeting() {
     var meetDetails = document.getElementById("meetDetails");
@@ -10,5 +16,14 @@ function joinMeeting() {
         createNewMeeting();
         return;
     }
-    chrome.tabs.create({url : "http://meet.google.com/lookup/" + meetDetails.value});
+    if(meetDetails.value.match(/^[a-z0-9]+$/i) != null) {
+        joinMeetingWithAlias(meetDetails.value);
+        return;
+    }
+    if(meetDetails.value.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/) != null) {
+        if(meetDetails.value.includes("meet.google.com"))
+        {
+            joinMeetingWithURL(meetDetails.value);
+        }
+    }
 }
