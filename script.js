d149a43c6e79b114a49cbdd6a4591cd04f8b0e77
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2022-01-29 17:26:27 +0530

    Added: Save Meet, Delete Saved Meets

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.getElementById("joinMeetForm").addEventListener("submit", joinMeeting);
+document.getElementById("joinMeetForm").addEventListener("submit", joinMeetingFromInput);
+document.getElementById("addMeetForm").addEventListener("submit", saveMeeting);
 var samePageMeetingLaunch = ["chrome://newtab", "edge://newtab", "about:blank"];
+var urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+var aliasRegex = /^[a-z0-9-]+$/i;
 
 function NewMeetingURL() {
     return "http://meet.google.com/new";
@@ -7,22 +10,24 @@ function NewMeetingURL() {
 function AliasToURL(alias) {
     return "http://meet.google.com/lookup/" + alias;
 }
-
 function getURL(userInput) {
     if (userInput.length == 0) {
         return NewMeetingURL();
     }
-    if (userInput.match(/^[a-z0-9-]+$/i) != null) {
+    if (isMeetAlias(userInput)) {
         return AliasToURL(userInput);
     }
-    if (userInput.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/) != null) {
-        if (userInput.includes("meet.google.com")) {
-            return userInput;
-        }
+    if (isMeetURL(userInput)) {
+        return userInput;
     }
     throw new Error("Invalid Input");
 }
-
+function isMeetAlias(str) {
+    return str.match(aliasRegex) != null;
+}
+function isMeetURL(str) {
+    return str.match(urlRegex) != null && str.includes("meet.google.com");
+}
 // this function checks if active tab is in samePageMeetingLaunch, if yes, url is opened in activeTab
 // if not, new tab is created with url
 function launchMeeting(url, tabPromise) {
@@ -39,13 +44,93 @@ function launchMeeting(url, tabPromise) {
         chrome.tabs.create({ url: url });
     });
 }
-function joinMeeting() {
+function joinMeeting(meetDetails) {
     var tabPromise = chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    var meetDetails = document.getElementById("meetDetails").value;
     try {
-        launchMeeting(url = getURL(meetDetails), tabPromise);
+        launchMeeting(getURL(meetDetails), tabPromise);
     }
     catch (err) {
         console.log(err);
     }
 }
+
+function deleteMeet(meetDetails, meetDescription) {
+    chrome.storage.local.get({ savedMeets: [] }, function (result) {
+        var savedMeets = result.savedMeets;
+        savedMeets = savedMeets.filter((meet) => meet.meetDetails != meetDetails || meet.meetDescription != meetDescription);
+        chrome.storage.local.set({ savedMeets: savedMeets }, function () {
+            chrome.storage.local.get('savedMeets', function (res) {
+                console.log(res.savedMeets)
+            });
+        });
+    });
+}
+
+function storeMeetDetails(meetDetails, meetDescription = "") {
+    if (meetDescription.length == 0) {
+        meetDescription = meetDetails;
+    }
+    chrome.storage.local.get({ savedMeets: [] }, function (result) {
+        var savedMeets = result.savedMeets;
+        savedMeets.push({ meetDetails:meetDetails, meetDescription: meetDescription });
+        chrome.storage.local.set({ savedMeets: savedMeets }, function () {
+            chrome.storage.local.get('savedMeets', function (res) {
+                console.log(res.savedMeets)
+            });
+        });
+    });
+}
+
+function joinMeetingFromInput() {
+    var meetDetails = document.getElementById("meetDetails").value;
+    joinMeeting(meetDetails);
+}
+
+function loadMeeting() {
+    chrome.storage.local.get({ savedMeets: [] }, function (res) {
+        var savedMeets = res.savedMeets;
+        var meetList = document.getElementById("meetList");
+        meetList.innerHTML = "";
+        savedMeets.forEach(function (meet) {
+            var li = document.createElement("li");
+
+            var a = document.createElement("a");
+            a.href = "";
+            a.innerText = meet.meetDescription;
+            li.appendChild(a);
+
+            var a1 = document.createElement("a");
+            a1.href = "";
+            a1.innerText = "Delete";
+            a1.addEventListener("click", function () {
+                deleteMeet(meet.meetDetails, meet.meetDescription);
+            });
+            li.appendChild(a1);
+
+            var a2 = document.createElement("a");
+            a2.href = "";
+            a2.innerText = "Join";
+            a2.addEventListener("click", function () {
+                joinMeeting(meet.meetDetails);
+            });
+            li.appendChild(a2);
+
+            meetList.appendChild(li);
+        });
+    });
+}
+
+function saveMeeting() {
+    var meetDetails = document.getElementById("saveMeetInp").value;
+    var meetDesc = document.getElementById("meetDesc").value;
+    if (meetDetails.length == 0) return;
+    try {
+        if (isMeetURL(meetDetails) || isMeetAlias(meetDetails)) {
+            storeMeetDetails(meetDetails, meetDesc);
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+window.onload = loadMeeting;
